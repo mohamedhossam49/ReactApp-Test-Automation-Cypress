876ecf0dbc7a88c51321dd336efaa199e253ca50
mohamedhossam49 <67132528+mohamedhossam49@users.noreply.github.com>
2021-10-13 00:37:27 +0200

    Test Reporting enhancement

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line import/prefer-default-export
+
 export const ignoreUncaughtException = () => {
   Cypress.on('uncaught:exception', () => false);
 };
--- reporter.js
+++ reporter.js
@@ -2,5 +2,5 @@ const report = require('multiple-cucumber-html-reporter');
 
 report.generate({
   jsonDir: './cypress/cucumber-json/',
-  reportPath: './cypress/cucumber-report/'
+  reportPath: "./reports/cucumber-htmlreport.html"
 });
